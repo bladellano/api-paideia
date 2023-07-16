68c3c0c231ba006d64d5d9dc8eeaefc972d26638
Caio <bladellano@gmail.com>
2023-07-16 12:00:59 +0100

    :wrench:Manutenção no relatório

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -8,54 +8,56 @@ class ReportController extends Controller
     {
         $length = $_REQUEST['length'];
         $start = $_REQUEST["start"];
+        $searchValue = $_GET['search']['value'];
+        $draw = $_REQUEST["draw"];
+        $order = $_REQUEST['order'][0];
 
-        $WHERE = " WHERE TRUE ";
+        $WHERE = "";
 
-        if(!empty($_REQUEST['search']['value'])):
-            $WHERE .= " AND s.name LIKE '".$_REQUEST['search']['value']."%' ";
-            $WHERE .= " OR t.name LIKE '".$_REQUEST['search']['value']."%' ";
-            $WHERE .= " OR p.name LIKE '".$_REQUEST['search']['value']."%' ";
-            $WHERE .= " OR p.responsible LIKE '".$_REQUEST['search']['value']."%' ";
-            $WHERE .= " OR g.name LIKE '".$_REQUEST['search']['value']."%' ";
+        if(!empty($searchValue)):
+            $WHERE .= " WHERE TRUE ";
+            $WHERE .= " AND s.name LIKE '".$searchValue."%' ";
+            $WHERE .= " OR t.name LIKE '".$searchValue."%' ";
+            $WHERE .= " OR p.name LIKE '".$searchValue."%' ";
+            $WHERE .= " OR p.responsible LIKE '".$searchValue."%' ";
+            $WHERE .= " OR g.name LIKE '".$searchValue."%' ";
         endif;
 
         $SQL = '
-        SELECT
-        s.id as student_id,
-        s.name,
-        s.email,
-        t.name as team,
-        p.name as polo,
-        p.responsible,
-        g.name as grid
-        FROM students s
-            LEFT JOIN (SELECT * FROM student_teams WHERE registered = 1) ST ON ST.student_id = s.id
-            LEFT JOIN teams t ON t.id = ST.team_id
-            LEFT JOIN polos p ON p.id = t.polo_id
-            LEFT JOIN grids g ON g.id = t.grid_id ';
+            SELECT
+            s.id as student_id,
+            s.name,
+            s.email,
+            t.name as team,
+            p.name as polo,
+            p.responsible,
+            g.name as grid
+            FROM students s
+                LEFT JOIN (SELECT * FROM student_teams WHERE registered = 1) ST ON ST.student_id = s.id
+                LEFT JOIN teams t ON t.id = ST.team_id
+                LEFT JOIN polos p ON p.id = t.polo_id
+                LEFT JOIN grids g ON g.id = t.grid_id ';
+
+        $SQL .= $WHERE;
 
         $SQL_TOTAL = $SQL;
 
-        $SQL .= $WHERE;
-        $SQL .= " ORDER BY ". ($_REQUEST['order'][0]['column'] + 1) . " " . strtoupper($_REQUEST['order'][0]['dir']);
-
         $LIMIT = " LIMIT " . $start . "," . $length;
-        $LIMIT = ($length < 0) ? "" : $LIMIT;
 
-        $records = \DB::select($SQL . $LIMIT);
+        $SQL .= " ORDER BY ". ($order['column'] + 1) . " " . strtoupper($order['dir']);
+        $SQL .= $LIMIT;
 
-        $totalQuery  = count(\DB::select($SQL_TOTAL));
-        $lengthQuery = count($records);
+        $records = \DB::select($SQL);
+
+        $totalRecords  = count(\DB::select($SQL_TOTAL));
 
         $response = [];
-        $response["draw"] = $_REQUEST["draw"];
-        $response["length"] = $lengthQuery;
-        $response["recordsTotal"] = $totalQuery;
-        $response["recordsFiltered"] = $totalQuery;
+        $response["draw"] = intval($draw);
+        $response["recordsTotal"] = $totalRecords;
+        $response["recordsFiltered"] = $totalRecords;
         $response["data"] = $records;
 
         return $response;
-
     }
 
 }
