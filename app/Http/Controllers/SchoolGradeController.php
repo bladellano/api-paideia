8a5dcafa647955c612af3dc4e27f44d7547569c0
Caio <bladellano@gmail.com>
2024-03-26 11:04:12 +0000

    :hammer:Transferindo o processo de criar notas para SchoolGradeService

--- app/Http/Controllers/SchoolGradeController.php
+++ app/Http/Controllers/SchoolGradeController.php
@@ -17,55 +17,15 @@ class SchoolGradeController extends Controller
     public function store(Request $request)
     {
 
-        $arLines = $this->generateHistoryLines($request->all());
-
-        $teamId = $arLines[0]['team_id'];
-        $studentId = $arLines[0]['student_id'];
-
-        $record = SchoolGrade::where('student_id', $studentId)
-            ->where('team_id', $teamId)
-            ->get();
-
-        if (!$record->isEmpty()) {
-
-            SchoolGrade::where('student_id', $studentId)
-                ->where('team_id', $teamId)
-                ->delete();
-        }
-
         try {
 
-            foreach ($arLines as $row)
-                SchoolGrade::create($row);
+            $grades = (new \App\Services\SchoolGradeService)->execute($request);
 
-            return response()->json(['data' => ['total' => count($arLines)], 'message' => 'Nota(s) criado/atualizado com sucesso!'], Response::HTTP_CREATED);
+            return response()->json(['data' => ['total' => $grades['total']], 'message' => 'Nota(s) criado/atualizado com sucesso!'], Response::HTTP_CREATED);
         } catch (\Exception $e) {
 
-            return response()->json(['error' => true, 'message' => 'Erro ao criar a(s) nota(s): ' . $e->getMessage()], Response::HTTP_BAD_REQUEST);
+            return response()->json(['error' => true, 'message' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
         }
-    }
-
-    public function generateHistoryLines($data)
-    {
-        $rows = [];
-
-        foreach ($data['grade'] as $studentId => $gradeData) {
-            foreach ($gradeData as $stageId => $subGradeData) {
-                foreach ($subGradeData as $disciplineId => $subSubGradeData) {
-                    foreach ($subSubGradeData as $turmaId => $grade) {
-                        $rows[] = [
-                            'grade' => $grade ?? 0,
-                            'student_id' => $studentId,
-                            'stage_id' => $stageId,
-                            'discipline_id' => $disciplineId,
-                            'team_id' => $turmaId,
-                        ];
-                    }
-                }
-            }
-        }
-
-        return $rows;
     }
 
     public function getGradeByStudent($studentId)
---
+++ app/Services/SchoolGradeService.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\SchoolGrade;
+use Illuminate\Http\Request;
+
+class SchoolGradeService
+{
+    
+    /**
+     * Execute the function with the given request.
+     *
+     * @param Request $request The request data
+     * @throws \Exception Error creating grade(s): message
+     * @return array
+     */
+    public function execute(Request $request)
+    {
+        $arLines = self::generateHistoryLines($request->all());
+        
+        $teamId = $arLines[0]['team_id'];
+        $studentId = $arLines[0]['student_id'];
+        
+        SchoolGrade::where('student_id', $studentId)
+        ->where('team_id', $teamId)
+        ->delete();
+        
+        try {
+            SchoolGrade::insert($arLines);
+            
+            return ['total' => count($arLines)];
+            
+        } catch (\Exception $e) {
+            throw new \Exception('Error creating grade(s): ' . $e->getMessage());
+        }
+    }
+    
+    private static function generateHistoryLines($data)
+    {
+        $rows = [];
+        
+        foreach ($data['grade'] as $studentId => $gradeData) {
+            foreach ($gradeData as $stageId => $subGradeData) {
+                foreach ($subGradeData as $disciplineId => $subSubGradeData) {
+                    foreach ($subSubGradeData as $turmaId => $grade) {
+                        $rows[] = [
+                            'grade' => $grade ?? 0,
+                            'student_id' => $studentId,
+                            'stage_id' => $stageId,
+                            'discipline_id' => $disciplineId,
+                            'team_id' => $turmaId,
+                        ];
+                    }
+                }
+            }
+        }
+        
+        return $rows;
+    }
+}
