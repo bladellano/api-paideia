38282a832f8dcb5708fb1fb7d1bfcfc4b9547c16
Caio <bladellano@gmail.com>
2024-05-17 16:57:20 +0100

    :hammer:Adicionando uma borda no recibo

--- resources/views/export/receipt.blade.php
+++ resources/views/export/receipt.blade.php
@@ -1,25 +1,32 @@
 
-<div style="text-align: center">
-    <img src="{{public_path('/logo.png')}}" alt="LOGO - PAIDEIA" width="220px">
-    <h3>RECIBO DE PAGAMENTO</h3>
 
-</div>
-
-<p>Recebi(emos) de: <strong>{{ strtoupper($financial->registration->student->name) }}</strong> a importância de R$ {{ $financial->valueFormated }} ({{ $financial->inFull }}) referente a(o) {{ strtoupper($financial->serviceType->name) }} de {{ $financial->due_date->translatedFormat('d \d\e F \d\e Y') }}.</p>
-
-<p>{{ $financial->course }}</p>
-
-<p>Para maior clareza firmo (amos) o presente recibo para que produza os seus efeitos, dando plena, rasa e irrevogável quitação, pelo valor recebido.</p>
-
-<p><strong>Obs: {{ $financial->observations }}</strong></p>
-
-<p>{{ ucfirst($financial->currentDate->translatedFormat('l, d \d\e F \d\e Y')) }}.</p>
-
-
-
-<div style="text-align: center">
-    <p>____________________________________________</p>
-    Paideia Educacional<br/>
-    CNPJ 32.599.936/0001-77
-
-</div>
+<table>
+    <tr>
+        <td border="1" style="border:1px solid #000; padding:10px">
+            <div style="text-align: center">
+                <img src="{{public_path('/logo.png')}}" alt="LOGO - PAIDEIA" width="220px">
+                <h3>RECIBO DE PAGAMENTO</h3>
+            
+            </div>
+            
+            <p>Recebi(emos) de: <strong>{{ strtoupper($financial->registration->student->name) }}</strong> a importância de R$ {{ $financial->valueFormated }} ({{ $financial->inFull }}) referente a(o) {{ strtoupper($financial->serviceType->name) }} de {{ $financial->due_date->translatedFormat('d \d\e F \d\e Y') }}.</p>
+            
+            <p>{{ $financial->course }}</p>
+            
+            <p>Para maior clareza firmo (amos) o presente recibo para que produza os seus efeitos, dando plena, rasa e irrevogável quitação, pelo valor recebido.</p>
+            
+            <p><strong>Obs: {{ $financial->observations }}</strong></p>
+            
+            <p>{{ ucfirst($financial->currentDate->translatedFormat('l, d \d\e F \d\e Y')) }}.</p>
+            
+            
+            
+            <div style="text-align: center">
+                <p>____________________________________________</p>
+                Paideia Educacional<br/>
+                CNPJ 32.599.936/0001-77
+            
+            </div>
+        </td>
+    </tr>
+</table>
