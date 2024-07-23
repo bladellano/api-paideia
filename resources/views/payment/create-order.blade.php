fce6a91d9cd4f3e21b6c60a5793a4014e0525865
Caio <bladellano@gmail.com>
2024-07-23 14:00:54 +0100

    [pagarme] Melhorando a interface para preencher dados do cartao

--- resources/views/payment/create-order.blade.php
+++ resources/views/payment/create-order.blade.php
@@ -1,9 +1,27 @@
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>Efetuar Pagamento</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        .input-icon {
+            position: relative;
+        }
+        .input-icon input {
+            padding-left: 40px; /* Espaço para o ícone */
+        }
+        .input-icon .icon {
+            position: absolute;
+            left: 10px;
+            top: 64%;
+            transform: translateY(-50%);
+            pointer-events: none;
+        }
+
+        fieldset {
+            background: azure;
+        }
+    </style>
 </head>
 <body>
 <div class="container mt-5 mb-5">
@@ -31,13 +49,9 @@
         </div>
 
         <div class="row">
-
             <div class="col-md-6">
-            
                 <h4>Informações do Cliente</h4>
-
                 <input type="hidden" name="code" value="{{ $financial->id }}">
-
                 <div class="form-group">
                     <label for="customer_name">Nome</label>
                     <input type="text" class="form-control" readonly value="{{ $financial->registration->student->name }}" name="customer[name]" required>
@@ -53,7 +67,6 @@
                     <input type="hidden" value="individual" name="customer[type]">
                 </div>
                 <div class="form-group">
-                    
                     @php
                         $area_code = preg_replace('/[^0-9]/', '', substr($financial->registration->student->phone, 1, 3)); 
                         $phone = str_replace('-', '', substr($financial->registration->student->phone, 4)); 
@@ -69,12 +82,10 @@
             </div>
 
             <div class="col-md-6">
-
                 <h4>Endereço de Cobrança</h4>
                 
                 <div class="form-group">
                     <label for="billing_address_line_1">Endereço</label>
-
                     <input 
                         type="text" 
                         class="form-control" 
@@ -95,7 +106,6 @@
                 </div>
               
                 <div class="row">
-
                     <div class="form-group col-md-6">
                         <label for="billing_country">País</label>
                         <select class="form-control" name="payments[0][credit_card][card][billing_address][country]" required>
@@ -112,50 +122,52 @@
                                 @endforeach
                         </select>
                     </div>
-
                 </div>
 
                 <div class="form-group">
                     <label for="billing_city">Cidade</label>
                     <input type="text" class="form-control" name="payments[0][credit_card][card][billing_address][city]" required>
                 </div>
+            </div>
+        </div>
 
+        <fieldset class="border p-3 mt-4">
+
+            <input type="hidden" name="payments[0][payment_method]" value="credit_card">
+
+            <div class="form-group input-icon mb-3">
+                <label for="card_number">Número do Cartão</label>
+                <span class="icon"><img src="{{ asset('images/credit-card.png') }}" alt="Card Icon" width="20"></span>
+                <input type="text" class="form-control" name="payments[0][credit_card][card][number]" required placeholder="0000000000000000" oninput="this.value = this.value.replace(/\s+/g, '')">
+            </div>
+            <div class="form-group input-icon mb-3">
+                <label for="card_holder_name">Nome no Cartão</label>
+                <span class="icon"><img src="{{ asset('images/do-utilizador.png') }}" alt="User Icon" width="20"></span>
+                <input type="text" class="form-control" name="payments[0][credit_card][card][holder_name]" required value="{{ mb_strtoupper($financial->registration->student->name) }}">
+            </div>
+            <div class="form-group input-icon mb-3">
+                <label for="card_exp_month">Mês de Expiração</label>
+                <span class="icon"><img src="{{ asset('images/calendar.png') }}" alt="Calendar Icon" width="20"></span>
+                <input type="text" class="form-control" name="payments[0][credit_card][card][exp_month]" required placeholder="00">
+            </div>
+            <div class="form-group input-icon mb-3">
+                <label for="card_exp_year">Ano de Expiração</label>
+                <span class="icon"><img src="{{ asset('images/calendar.png') }}" alt="Calendar Icon" width="20"></span>
+                <input type="text" class="form-control" name="payments[0][credit_card][card][exp_year]" required placeholder="00">
+            </div>
+            <div class="form-group input-icon mb-3">
+                <label for="card_cvv">CVV</label>
+                <span class="icon"><img src="{{ asset('images/cvv.png') }}" alt="CVV Icon" width="20"></span>
+                <input type="text" class="form-control" name="payments[0][credit_card][card][cvv]" required placeholder="000">
             </div>
 
-        </div>
+            <input type="hidden" name="payments[0][credit_card][recurrence]" value="false">
+            <input type="hidden" name="payments[0][credit_card][installments]" value="1">
+            <input type="hidden" name="payments[0][credit_card][statement_descriptor]" value="{{ $financial->observations }}">
+        </fieldset>
 
-        <h4>Informações do Cartão</h4>
-
-        <input type="hidden" name="payments[0][payment_method]" value="credit_card">
-
-        <div class="form-group">
-            <label for="card_number">Número do Cartão</label>
-            <input type="text" class="form-control" name="payments[0][credit_card][card][number]" required placeholder="0000000000000000" oninput="this.value = this.value.replace(/\s+/g, '')">
-        </div>
-        <div class="form-group">
-            <label for="card_holder_name">Nome no Cartão</label>
-            <input type="text" class="form-control" name="payments[0][credit_card][card][holder_name]" required value="{{ mb_strtoupper($financial->registration->student->name) }}">
-        </div>
-        <div class="form-group">
-            <label for="card_exp_month">Mês de Expiração</label>
-            <input type="text" class="form-control" name="payments[0][credit_card][card][exp_month]" required placeholder="00">
-        </div>
-        <div class="form-group">
-            <label for="card_exp_year">Ano de Expiração</label>
-            <input type="text" class="form-control" name="payments[0][credit_card][card][exp_year]" required placeholder="00">
-        </div>
-        <div class="form-group">
-            <label for="card_cvv">CVV</label>
-            <input type="text" class="form-control" name="payments[0][credit_card][card][cvv]" required placeholder="000">
-        </div>
-
-        <input type="hidden" name="payments[0][credit_card][recurrence]" value="false">
-        <input type="hidden" name="payments[0][credit_card][installments]" value="1">
-        <input type="hidden" name="payments[0][credit_card][statement_descriptor]" value="{{ $financial->observations }}">
-
-        <button type="submit" class="btn btn-primary">Efetuar Pagamento</button>
+        <button type="submit" class="btn btn-primary mt-3">Efetuar Pagamento</button>
     </form>
 </div>
-
 </body>
 </html>
